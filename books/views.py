2f34641157c35366a3d2c6252b9149095be7a3bd
Tracy Somervell <tsomerve@gmail.com>
2018-08-27 20:50:43 -0700

    added pages and views

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
+class HomePageView(TemplateView):
+    template_name = 'books/home.html'
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class AboutPageView(TemplateView):
+    template_name = 'books/about.html'
