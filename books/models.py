48ea9cbf8c24bb257aebae3ce99ffe560f6a1fbe
Tracy Somervell <tsomerve@gmail.com>
2018-08-28 19:55:37 -0700

    added models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Checkout(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.text[:50]
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=60)
+
+class Book(models.Model):
+    
--- books/views.py
+++ books/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Checkout
 
 class HomePageView(TemplateView):
     template_name = 'books/home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'books/about.html'
+
